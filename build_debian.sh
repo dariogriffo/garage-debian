d80a1feff695ecfe02fc676337d6e12b9f6a1c3c
Dario Griffo <dariogriffo@gmail.com>
2026-07-09 15:31:30 +0100

    Use ~ instead of + for the distro release suffix

--- build_debian.sh
+++ build_debian.sh
@@ -86,7 +86,7 @@ build_architecture() {
     declare -a arr=("bookworm" "trixie" "forky" "sid")
 
     for dist in "${arr[@]}"; do
-        FULL_VERSION="$garage_VERSION-${BUILD_VERSION}+${dist}_${build_arch}"
+        FULL_VERSION="$garage_VERSION-${BUILD_VERSION}~${dist}_${build_arch}"
         echo "  Building $FULL_VERSION"
 
         if ! docker build . -t "garage-$dist-$build_arch" \
